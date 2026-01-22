b2f6414a3668191f0253b811250aca6967d48df3
Allen Goodman <allen.goodman@icloud.com>
2026-01-22 16:44:26 -0500

    test(linear_algebra): add decomposition test directory

---
+++ tests/torchscience/linear_algebra/decomposition/test__symmetric_generalized_eigenvalue.py
@@ -0,0 +1,9 @@
+"""Tests for symmetric_generalized_eigenvalue decomposition."""
+
+
+class TestSymmetricGeneralizedEigenvalue:
+    """Tests for symmetric generalized eigenvalue decomposition."""
+
+    def test_placeholder(self):
+        """Placeholder test - will be replaced with real tests."""
+        assert True
