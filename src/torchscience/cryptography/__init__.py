8b72a8125ff6d7700aef1b8283a2d21708464d43
Allen Goodman <allen.goodman@icloud.com>
2026-01-22 21:18:57 -0500

    feat(cryptography): add BLAKE2 Python wrappers
    Add blake2b and blake2s functions with docstrings and export from
    the cryptography module.

--- src/torchscience/cryptography/__init__.py
+++ src/torchscience/cryptography/__init__.py
@@ -1,3 +1,4 @@
+from torchscience.cryptography._blake2 import blake2b, blake2s
 from torchscience.cryptography._chacha20 import chacha20
 from torchscience.cryptography._generator import ChaCha20Generator
 from torchscience.cryptography._hmac import hmac_sha256
@@ -5,6 +6,8 @@ from torchscience.cryptography._sha3 import keccak256, sha3_256, sha3_512
 from torchscience.cryptography._sha256 import sha256
 
 __all__ = [
+    "blake2b",
+    "blake2s",
     "chacha20",
     "ChaCha20Generator",
     "hmac_sha256",
