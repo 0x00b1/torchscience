b7896570023dcbacf608bf88d9fc9b8c4567b448
Allen Goodman <allen.goodman@icloud.com>
2026-01-23 22:28:59 -0500

    feat(window): add C10_HOST_DEVICE to hann kernel for CUDA support

--- src/torchscience/csrc/kernel/signal_processing/window_function/hann.h
+++ src/torchscience/csrc/kernel/signal_processing/window_function/hann.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <c10/macros/Macros.h>
 #include "common.h"
 
 namespace torchscience::kernel::window_function {
@@ -8,7 +9,7 @@ namespace torchscience::kernel::window_function {
 // Hann window: w[k] = 0.5 * (1 - cos(2*pi*k / denom))
 // where denom = n-1 (symmetric) or n (periodic)
 template<typename scalar_t>
-inline scalar_t hann(int64_t i, int64_t n, bool periodic) {
+C10_HOST_DEVICE inline scalar_t hann(int64_t i, int64_t n, bool periodic) {
   if (n == 1) {
     return scalar_t(1);
   }
