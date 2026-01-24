9cf1c1ca6656924cc2844dd707c345e97483a297
Allen Goodman <allen.goodman@icloud.com>
2026-01-23 21:33:35 -0500

    feat(compression): add EntropyBottleneck for learned compression

--- src/torchscience/compression/__init__.py
+++ src/torchscience/compression/__init__.py
@@ -3,6 +3,7 @@
 from ._arithmetic import arithmetic_decode, arithmetic_encode
 from ._distortion_measure import distortion_measure
 from ._dithered_quantize import dithered_quantize
+from ._entropy_bottleneck import EntropyBottleneck, entropy_bottleneck
 from ._huffman import huffman_decode, huffman_encode
 from ._lattice_quantize import lattice_quantize
 from ._lz77 import lz77_decode, lz77_encode
@@ -23,6 +24,8 @@ __all__ = [
     "arithmetic_encode",
     "distortion_measure",
     "dithered_quantize",
+    "EntropyBottleneck",
+    "entropy_bottleneck",
     "estimate_bitrate",
     "huffman_decode",
     "huffman_encode",
