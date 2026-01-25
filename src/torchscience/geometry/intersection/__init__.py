8aca0563b3847e8dea34faf3105dff5dd22d36a1
Allen Goodman <allen.goodman@icloud.com>
2026-01-25 15:54:06 -0500

    feat(geometry): add ray_plane Python API

--- src/torchscience/geometry/intersection/__init__.py
+++ src/torchscience/geometry/intersection/__init__.py
@@ -1,10 +1,12 @@
 """Ray-geometry intersection primitives."""
 
 from ._ray_hit import IntersectionResult
+from ._ray_plane import ray_plane
 from ._ray_sphere_intersection import RaySphereHit, ray_sphere_intersection
 
 __all__ = [
     "IntersectionResult",
     "RaySphereHit",
+    "ray_plane",
     "ray_sphere_intersection",
 ]
