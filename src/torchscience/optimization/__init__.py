576674c5f1e24107d3082099d8f28b8bbd5b6a5c
Allen Goodman <allen.goodman@icloud.com>
2026-01-26 17:00:48 -0500

    feat(optimization): export test_function submodule

--- src/torchscience/optimization/__init__.py
+++ src/torchscience/optimization/__init__.py
@@ -1,8 +1,9 @@
-from . import linear, optim
+from . import linear, optim, test_function
 from ._result import OptimizeResult
 
 __all__ = [
     "OptimizeResult",
     "linear",
     "optim",
+    "test_function",
 ]
