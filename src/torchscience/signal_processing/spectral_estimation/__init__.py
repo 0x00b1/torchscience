537a1393d5729e5a07328818724c77da7ae31951
Allen Goodman <allen.goodman@icloud.com>
2026-01-30 15:49:34 -0500

    feat(signal_processing): add spectrogram with tests

--- src/torchscience/signal_processing/spectral_estimation/__init__.py
+++ src/torchscience/signal_processing/spectral_estimation/__init__.py
@@ -1,9 +1,11 @@
 """Spectral estimation operations."""
 
 from ._periodogram import periodogram
+from ._spectrogram import spectrogram
 from ._welch import welch
 
 __all__ = [
     "periodogram",
+    "spectrogram",
     "welch",
 ]
