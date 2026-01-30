393aa816b65ed167fbeaec421929526d953101c6
Allen Goodman <allen.goodman@icloud.com>
2026-01-30 16:13:27 -0500

    feat(transform): add z_transform CUDA backend

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -27,6 +27,8 @@ set(TORCHSCIENCE_CUDA_SOURCES
   src/torchscience/csrc/cuda/transform/fourier_sine_transform.cu
   src/torchscience/csrc/cuda/transform/inverse_fourier_sine_transform.cu
   src/torchscience/csrc/cuda/transform/convolution.cu
+  src/torchscience/csrc/cuda/transform/z_transform.cu
+  src/torchscience/csrc/cuda/transform/inverse_z_transform.cu
 )
 
 # Add CUDA sources to target
