3f95d4ff1613bf46a7419b5c6cc72fdbb56698a6
Allen Goodman <allen.goodman@icloud.com>
2026-01-31 19:40:36 -0500

    feat(special_functions): add hypergeometric_1_f_2 Python wrapper

--- src/torchscience/special_functions/__init__.py
+++ src/torchscience/special_functions/__init__.py
@@ -39,6 +39,7 @@ from ._exponential_integral_ei import exponential_integral_ei
 from ._exponential_integral_ein import exponential_integral_ein
 from ._gamma import gamma
 from ._hypergeometric_0_f_1 import hypergeometric_0_f_1
+from ._hypergeometric_1_f_2 import hypergeometric_1_f_2
 from ._hypergeometric_2_f_1 import hypergeometric_2_f_1
 from ._incomplete_beta import incomplete_beta
 from ._incomplete_legendre_elliptic_integral_e import (
@@ -178,6 +179,7 @@ __all__ = [
     "exponential_integral_ein",
     "gamma",
     "hypergeometric_0_f_1",
+    "hypergeometric_1_f_2",
     "hypergeometric_2_f_1",
     "incomplete_beta",
     "log_beta",
